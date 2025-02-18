4f20dff2e265b155764562b86102ca5c0c3ecd47
Charles Ponti <cj@ponti.io>
2025-02-18 12:42:23 -0800

    refactor(*): update project config files

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,8 +1,7 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-	"./apps/api/vitest.config.ts",
-	"./apps/hominem-api/vitest.config.mts",
+	"./apps/api/vitest.config.mts",
 	"./packages/utils/vitest.config.mts",
 	"./scratchpad/vitest.config.mts",
 ]);
