4a050bfe87fe75b341503c611e0014d1f13d6c5a
Charles Ponti <cj@ponti.io>
2025-03-10 19:40:21 -0700

    feat: update environment variable loading and remove start script from package.json

--- apps/cli/src/env.ts
+++ apps/cli/src/env.ts
@@ -1,6 +1,8 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import os from 'node:os'
 import path from 'node:path'
+
+dotenv.config({ path: path.resolve(os.homedir(), '.hominem/.env') })
 
 export const env = {
   DB_PATH: path.resolve(`${os.homedir()}/.hominem/db.sqlite`),
