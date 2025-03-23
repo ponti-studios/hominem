22b566c56e45dc6cf25eddeb6791b55282bc9cde
Charles Ponti <charlesponti@users.noreply.github.com>
2025-03-23 08:51:39 -0700

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import Providers from '@/components/providers'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { auth } from '@clerk/nextjs/server'
 import { Menu } from 'lucide-react'
+import Head from 'next/head';
 import { SiteNavigation } from '../components/app-navigation'
 import './globals.css'
 
@@ -14,6 +15,16 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        {/* Basic Meta Tags */}
+        <title>hominem</title>
+        <meta name="description" content="manage life easier." />
+        <meta name="keywords" content="nextjs, react, example, keywords" />
+        <meta name="author" content="ponti-studios" />
+
+        {/* Viewport */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </Head>
       <body>
         <Providers>
           <div className="bg-background text-foreground min-h-screen min-w-full flex max-h-screen">
