e0c91986a7fcd38ba56c7bcc4fc029f6e581feb3
Charles Ponti <cj@ponti.io>
2025-03-24 14:01:13 -0700

    chore: remove streamyard code tests

--- apps/streamyard/challenge-hidden-videos/jest.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  testRegex: '((\\.|/*.)(spec))\\.js?$',
-}
--- apps/streamyard/challenge-screenshare/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['danbriggs5-react', 'plugin:prettier/recommended'],
-  parser: 'babel-eslint',
-  rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'no-console': 'off',
-  },
-}
