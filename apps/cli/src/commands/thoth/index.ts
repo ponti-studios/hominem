3aa091bcf7fc42d233879119319426d97eea8f95
Charles Ponti <cj@ponti.io>
2025-04-10 13:09:25 -0700

    feat: remove query-json command

--- apps/cli/src/commands/thoth/index.ts
+++ apps/cli/src/commands/thoth/index.ts
@@ -1,7 +1,6 @@
 import { Command } from 'commander'
 import processMarkdownCommand from './process-markdown'
 import processMarkdownChunkCommand from './process-markdown-chunks'
-import { queryJsonCommand } from './query-json'
 
 const program = new Command()
 
@@ -9,6 +8,5 @@ program.name('thoth').description('Writing tools')
 
 program.addCommand(processMarkdownCommand)
 program.addCommand(processMarkdownChunkCommand)
-program.addCommand(queryJsonCommand)
 
 export default program
