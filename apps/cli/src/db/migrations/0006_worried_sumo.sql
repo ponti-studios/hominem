860c16d5c582296fbad2dde4056116214e2db237
Charles Ponti <cj@ponti.io>
2025-04-21 15:41:26 -0700

    fix: update cli migrations

---
+++ apps/cli/src/db/migrations/0006_worried_sumo.sql
@@ -0,0 +1,4 @@
+DROP TABLE `accounts`;--> statement-breakpoint
+DROP TABLE `transaction_accounts`;--> statement-breakpoint
+DROP TABLE `transaction_names`;--> statement-breakpoint
+DROP TABLE `transactions`;
