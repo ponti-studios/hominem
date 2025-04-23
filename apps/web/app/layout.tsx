c6dcc0a077d41b72b566717bb322caadeb29ce16
Charles Ponti <cj@ponti.io>
2025-04-23 09:30:48 -0700

    style: enable main layout container to scroll

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -30,8 +30,8 @@ export default async function RootLayout({
         <Providers>
           <div className="bg-background text-foreground min-h-screen min-w-full flex flex-col max-h-screen overflow-x-hidden">
             <MainNavigation />
-            <main className="flex-1 overflow-y-auto pt-4 pb-safe-area-inset-bottom">
-              <div className="md:container md:mx-auto md:h-[calc(100dvh-85px)] px-4 overflow-x-hidden">
+            <main className="flex-1 overflow-y-auto pt-4">
+              <div className="md:container md:mx-auto md:h-[calc(100dvh-85px)] px-4">
                 {children}
               </div>
             </main>
