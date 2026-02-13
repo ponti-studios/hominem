f4a8e09d9488c191af75b4155041050fab3c236a
Charles Ponti <1757624+charlesponti@users.noreply.github.com>
2026-02-12 20:16:33 -0800

    Fix missing @hominem/db imports

--- apps/rocco/app/lib/hooks/use-admin.ts
+++ apps/rocco/app/lib/hooks/use-admin.ts
@@ -4,7 +4,9 @@ import type {
   AdminRefreshGooglePlacesOutput,
 } from '@hominem/hono-rpc/types/admin.types';
 
-import { useHonoMutation } from '@hominem/hono-client/react';
+import { useHonoMutation, useHonoUtils } from '@hominem/hono-client/react';
+
+import { queryKeys } from '~/lib/query-keys';
 
 /**
  * Refresh Google Places data
@@ -12,11 +14,32 @@ import { useHonoMutation } from '@hominem/hono-client/react';
 export const useRefreshGooglePlaces = (
   options?: HonoMutationOptions<AdminRefreshGooglePlacesOutput, AdminRefreshGooglePlacesInput>,
 ) => {
+  const utils = useHonoUtils();
   return useHonoMutation<AdminRefreshGooglePlacesOutput, AdminRefreshGooglePlacesInput>(
     async (client, variables: AdminRefreshGooglePlacesInput) => {
       const res = await client.api.admin['refresh-google-places'].$post({ json: variables });
       return (await res.json()) as unknown as AdminRefreshGooglePlacesOutput;
     },
-    options,
+    {
+      ...options,
+      onMutate: async () => {
+        await utils.cancel(queryKeys.places.all());
+        const previousPlaces = utils.getData(queryKeys.places.all());
+        return { previousPlaces };
+      },
+      onSuccess: (result, variables, context, mutationContext) => {
+        utils.invalidate(queryKeys.places.all());
+        options?.onSuccess?.(result, variables, context, mutationContext);
+      },
+      onError: (error, variables, context, mutationContext) => {
+        if (context && typeof context === 'object' && 'previousPlaces' in context) {
+          utils.setData(queryKeys.places.all(), (context as { previousPlaces: unknown }).previousPlaces);
+        }
+        options?.onError?.(error, variables, context, mutationContext);
+      },
+      onSettled: () => {
+        utils.invalidate(queryKeys.places.all());
+      },
+    },
   );
 };
--- apps/rocco/app/lib/hooks/use-user.ts
+++ apps/rocco/app/lib/hooks/use-user.ts
@@ -1,13 +1,89 @@
 import type { HonoClient } from '@hominem/hono-client';
+import type { HonoMutationOptions } from '@hominem/hono-client/react';
 import type { UserDeleteAccountOutput } from '@hominem/hono-rpc/types/user.types';
 
-import { useHonoMutation } from '@hominem/hono-client/react';
+import { useHonoMutation, useHonoUtils } from '@hominem/hono-client/react';
+
+import { queryKeys } from '~/lib/query-keys';
 
 /**
  * Delete user account
  */
-export const useDeleteAccount = (options?: any) =>
-  useHonoMutation<UserDeleteAccountOutput, {}>(async (client: HonoClient) => {
-    const res = await client.api.user['delete-account'].$post({ json: {} });
-    return (await res.json()) as unknown as UserDeleteAccountOutput;
-  }, options);
+export const useDeleteAccount = (options?: HonoMutationOptions<UserDeleteAccountOutput, {}>) => {
+  const utils = useHonoUtils();
+  return useHonoMutation<UserDeleteAccountOutput, {}>(
+    async (client: HonoClient) => {
+      const res = await client.api.user['delete-account'].$post({ json: {} });
+      return (await res.json()) as unknown as UserDeleteAccountOutput;
+    },
+    {
+      ...options,
+      onMutate: async () => {
+        await utils.cancel(queryKeys.lists.all());
+        await utils.cancel(queryKeys.places.all());
+        await utils.cancel(queryKeys.trips.all());
+        await utils.cancel(queryKeys.people.list());
+        await utils.cancel(queryKeys.invites.sent());
+        await utils.cancel(queryKeys.invites.received());
+
+        const previousLists = utils.getData(queryKeys.lists.all());
+        const previousPlaces = utils.getData(queryKeys.places.all());
+        const previousTrips = utils.getData(queryKeys.trips.all());
+        const previousPeople = utils.getData(queryKeys.people.list());
+        const previousInvitesSent = utils.getData(queryKeys.invites.sent());
+        const previousInvitesReceived = utils.getData(queryKeys.invites.received());
+
+        utils.setData(queryKeys.lists.all(), []);
+        utils.setData(queryKeys.places.all(), []);
+        utils.setData(queryKeys.trips.all(), []);
+        utils.setData(queryKeys.people.list(), []);
+        utils.setData(queryKeys.invites.sent(), []);
+        utils.setData(queryKeys.invites.received(), []);
+
+        return {
+          previousLists,
+          previousPlaces,
+          previousTrips,
+          previousPeople,
+          previousInvitesSent,
+          previousInvitesReceived,
+        };
+      },
+      onError: (error, _variables, context, mutationContext) => {
+        if (context && typeof context === 'object') {
+          if ('previousLists' in context) {
+            utils.setData(queryKeys.lists.all(), (context as any).previousLists);
+          }
+          if ('previousPlaces' in context) {
+            utils.setData(queryKeys.places.all(), (context as any).previousPlaces);
+          }
+          if ('previousTrips' in context) {
+            utils.setData(queryKeys.trips.all(), (context as any).previousTrips);
+          }
+          if ('previousPeople' in context) {
+            utils.setData(queryKeys.people.list(), (context as any).previousPeople);
+          }
+          if ('previousInvitesSent' in context) {
+            utils.setData(queryKeys.invites.sent(), (context as any).previousInvitesSent);
+          }
+          if ('previousInvitesReceived' in context) {
+            utils.setData(queryKeys.invites.received(), (context as any).previousInvitesReceived);
+          }
+        }
+
+        options?.onError?.(error, _variables, context, mutationContext);
+      },
+      onSuccess: (result, variables, context, mutationContext) => {
+        options?.onSuccess?.(result, variables, context, mutationContext);
+      },
+      onSettled: () => {
+        utils.invalidate(queryKeys.lists.all());
+        utils.invalidate(queryKeys.places.all());
+        utils.invalidate(queryKeys.trips.all());
+        utils.invalidate(queryKeys.people.list());
+        utils.invalidate(queryKeys.invites.sent());
+        utils.invalidate(queryKeys.invites.received());
+      },
+    },
+  );
+};
