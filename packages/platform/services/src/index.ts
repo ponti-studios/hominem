6e3826f5d7ab355b1ca1bbaf66681b0490e1ad1c
Charles Ponti <cj@ponti.io>
2026-04-14 23:34:22 -0700

    refactor: inline voice services into API router

--- packages/platform/services/src/index.ts
+++ packages/platform/services/src/index.ts
@@ -5,23 +5,3 @@ export * from './queues';
 export * from './redis';
 export * from './resend';
 export * from './types';
-export * from './voice-errors';
-export { generateSpeechBuffer } from './voice-speech.service';
-export {
-  generateVoiceResponse,
-  type VoiceResponseFormat,
-  type VoiceResponseInput,
-  type VoiceResponseOutput,
-  type VoiceResponseVoice,
-} from './voice-response.service';
-export {
-  VOICE_TRANSCRIPTION_MAX_SIZE_BYTES,
-  VOICE_TRANSCRIPTION_SUPPORTED_TYPES,
-  VOICE_TRANSPORTS,
-  getVoiceFileExtension,
-  normalizeVoiceMimeType,
-  transcribeVoiceBuffer,
-  validateVoiceInput,
-  type VoiceTranscriptionOutput,
-  type VoiceTransport,
-} from './voice-transcription.service';
